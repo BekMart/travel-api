65030b529ae15344c08a1c57524435d3da24a043
BekMart <bekmart1989@gmail.com>
2025-04-01 14:04:32 +0100

    Add location serializers/url path/list view

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Location
+from .serializers import LocationSerializer
 
-# Create your views here.
+
+class LocationList(generics.ListAPIView):
+    """
+    List all locations
+    """
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
