d45d602b6f8e3e33438b01c0af1416b93c88d1fa
BekMart <bekmart1989@gmail.com>
2025-04-01 12:11:45 +0100

    Create profiles model and link to admin
    Add function to create a new profile when user creates an account

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Profile model that extends the User model
+class Profile(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_profile_ngcscr'
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+
+
+# This function creates a profile for each user when the user is created.
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+# This saves the profile when the user is saved.
+post_save.connect(create_profile, sender=User)
