4864f3891c457311108d42e5f8c3e57565b04b07
BekMart <bekmart1989@gmail.com>
2025-04-01 13:43:40 +0100

    Create post/location models and register to admin

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from locations.models import Location
 
-# Create your models here.
+
+# Post model that represents a travel post
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_image_pr9kie'
+    )
+    location = models.ForeignKey(
+        Location, on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='posts'
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
