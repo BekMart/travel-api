316f07364889d627bc6b97901875b8ec3e39516c
BekMart <bekmart1989@gmail.com>
2025-04-01 18:01:55 +0100

    Add fields to location model

--- locations/models.py
+++ locations/models.py
@@ -5,6 +5,8 @@ from django.utils.text import slugify
 class Location(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True, blank=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
