96242b32f1e0c2f9464bf0ed5c4b81f8fe7b671b
BekMart <bekmart1989@gmail.com>
2025-04-01 22:36:10 +0100

    Create profile detail view and url path

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from travel_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
@@ -9,3 +10,12 @@ class ProfileList(generics.ListAPIView):
     """
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
+
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update a profile if you're the owner.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
