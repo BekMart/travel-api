4f5fd8bd0bc98b3f23e5504e43eda92a507c5140
BekMart <bekmart1989@gmail.com>
2025-04-02 10:57:02 +0100

    Add field to profile - shows user logged in/not

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,10 +5,18 @@ from .models import Profile
 # Serializer for the Profile model
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+    def get_is_owner(self, obj):
+        """
+        Check if the current user is the owner of the profile.
+        """
+        request = self.context.get('request')
+        return request and request.user == obj.owner
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'name', 'content', 'image', 'nationality', 'age',
-            'resides', 'created_on', 'updated_on'
+            'resides', 'created_on', 'updated_on', 'is_owner',
         ]
