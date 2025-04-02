975932970c83de54ecc0ea0a4b542e9410765b07
BekMart <bekmart1989@gmail.com>
2025-04-02 13:05:38 +0100

    CRUD functionality for likes

--- likes/views.py
+++ likes/views.py
@@ -1,11 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from travel_api.permissions import IsOwnerOrReadOnly
 from .models import Like
 from .serializers import LikeSerializer
 
 
-class LikeList(generics.ListAPIView):
+class LikeList(generics.ListCreateAPIView):
     """
-    List all likes
+    List all likes or create if logged in.
     """
     queryset = Like.objects.all()
     serializer_class = LikeSerializer
+    permmission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        """
+        Save the like with the logged in user as the owner.
+        """
+        serializer.save(owner=self.request.user)
+
+
+class LikeDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a like or delete it by id if you own it.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
