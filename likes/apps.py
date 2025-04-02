af7da8def1f48bdd16a93f208ea42c57ac3e2053
BekMart <bekmart1989@gmail.com>
2025-04-02 16:58:51 +0100

    Like notification setup

--- likes/apps.py
+++ likes/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class LikesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'likes'
+
+    def ready(self):
+        """
+        The ready method is called when the application is loaded.
+        Import the signals module to register the signal handlers.
+        """
+        import likes.signals
