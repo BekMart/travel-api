a12fbfa77a67ed6f1da651cddd26e849a53b4004
BekMart <bekmart1989@gmail.com>
2025-04-02 16:47:16 +0100

    Enable user to mark all notifcations as read

--- notifications/urls.py
+++ notifications/urls.py
@@ -3,4 +3,5 @@ from notifications import views
 
 urlpatterns = [
     path('notifications/', views.NotificationList.as_view()),
+    path('notifications/mark-read/', views.MarkAllNotificationsRead.as_view()),
 ]
