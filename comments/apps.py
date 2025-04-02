16875becbba400b45ff3cde67beb7c85bb92efcc
BekMart <bekmart1989@gmail.com>
2025-04-02 17:05:17 +0100

    Comment notification setup

--- comments/apps.py
+++ comments/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class CommentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'comments'
+
+    def ready(self):
+        """
+        The ready method is called when the application is loaded.
+        Import the signals module to register the signal handlers.
+        """
+        import comments.signals
