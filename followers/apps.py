12de1373b4bebcfb1ae000d401cff6dd7a035b7c
BekMart <bekmart1989@gmail.com>
2025-04-02 17:10:57 +0100

    Follow notification setup

--- followers/apps.py
+++ followers/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class FollowersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'followers'
+
+    def ready(self):
+        """
+        The ready method is called when the application is loaded.
+        Import the signals module to register the signal handlers.
+        """
+        import followers.signals
