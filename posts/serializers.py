c500d9c6c9841d782889186fb792b26f1eca701d
BekMart <bekmart1989@gmail.com>
2025-04-02 11:12:58 +0100

    Validation for uploaded images

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,6 +9,24 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        """
+        Validate the image size and dimensions.
+        """
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        return value
+
     def get_is_owner(self, obj):
         """
         Check if the current user is the owner of the post.
