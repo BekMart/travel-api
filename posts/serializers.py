bcf4b871a81f306b5cd18f5c332fc46019c1818d
BekMart <bekmart1989@gmail.com>
2025-04-02 14:20:06 +0100

    Users can create locations when submitting posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,13 +1,17 @@
 from rest_framework import serializers
 from .models import Post
+from locations.models import Location
 
 
-# Serializer for the Post model
 class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Post model
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    location = serializers.CharField()
 
     def validate_image(self, value):
         """
@@ -41,3 +45,27 @@ class PostSerializer(serializers.ModelSerializer):
             'created_on', 'updated_on', 'is_owner', 'profile_id',
             'profile_image',
         ]
+
+    def create(self, validated_data):
+        """
+        This method handles the creation of the location instance
+        if it does not exist.
+        """
+        location_name = validated_data.pop('location')
+        location, created = Location.objects.get_or_create(name=location_name)
+        validated_data['location'] = location
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update an existing post instance.
+        This method handles the update of the location instance
+        if the location name is provided.
+        """
+        location_name = validated_data.pop('location', None)
+        if location_name:
+            location, created = Location.objects.get_or_create(
+                name=location_name
+            )
+            validated_data['location'] = location
+        return super().update(instance, validated_data)
