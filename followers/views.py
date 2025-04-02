8173e843e5903fac4abd29fcf14c6e3dd212dc77
BekMart <bekmart1989@gmail.com>
2025-04-02 13:48:10 +0100

    Enable users to follow/unfollow others
    A user is limited to following each user once

--- followers/views.py
+++ followers/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from travel_api.permissions import IsOwnerOrReadOnly
 from .models import Follow
 from .serializers import FollowSerializer
 
@@ -8,5 +9,21 @@ class FollowList(generics.ListCreateAPIView):
     List all followers, i.e. all instances of a user
     following another user'.
     """
-    serializer_class = FollowSerializer
     queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        """
+        Save the follow with the logged in user as the owner.
+        """
+        serializer.save(owner=self.request.user)
+
+
+class FollowDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieve a follow, or unfllow by id if owner.
+    """
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [IsOwnerOrReadOnly]
