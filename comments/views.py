f6ffec066276ce8af6938c79d16c1e505bab38ef
BekMart <bekmart1989@gmail.com>
2025-04-02 12:54:42 +0100

    Enable users to retrieve/update/delete comments

--- comments/views.py
+++ comments/views.py
@@ -1,11 +1,28 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
+from travel_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
-from .serializers import CommentSerializer
+from .serializers import CommentSerializer, CommentDetailSerializer
 
 
 class CommentList(generics.ListCreateAPIView):
     """
     List comments or create a comment if logged in.
     """
+    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        """
+        Save the comment with the logged in user as the owner.
+        """
+        serializer.save(owner=self.request.user)
+
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a comment, or update or delete it by id if owner.
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
