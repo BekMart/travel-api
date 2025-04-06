5ece5e33e7c0f229ec8d39fb0fa832e49af6862d
BekMart <bekmart1989@gmail.com>
2025-04-06 17:38:20 +0100

    Add pagination

--- travel_api/settings.py
+++ travel_api/settings.py
@@ -26,7 +26,11 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 REST_FRAMEWORK = {
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # ✅ Load 10 items per page
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ]
 }
 
 # Quick-start development settings - unsuitable for production
