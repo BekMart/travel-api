d9fb07d419234fca2f066eaac79f0a89d6d375f6
BekMart <bekmart1989@gmail.com>
2025-04-06 20:05:38 +0100

    Prepare for deployment

--- env.py
+++ env.py
@@ -1,3 +1,5 @@
 import os
 os.environ['CLOUDINARY_URL'] = 'cloudinary://614471813872572:9U-t7FguDQTbk8omXoITJWEOQo0@dvgobcuck'
+os.environ['SECRET_KEY'] = 'znm@^^r824)n+5!si^w&czo&wtz98zt8vx44jo$c576kr-3885'
 os.environ['DEV'] = '1'
+os.environ['DATABASE_URL'] = "postgresql://neondb_owner:3yYAm7jMVXhE@ep-patient-night-a2vk2lfs.eu-central-1.aws.neon.tech/twine_spout_salt_334282"
