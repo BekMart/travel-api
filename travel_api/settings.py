1bd448d4c1bb461719a8eb9205a02f1e69704491
BekMart <bekmart1989@gmail.com>
2025-04-15 11:34:00 +0100

    Ammend settings to render with json

--- travel_api/settings.py
+++ travel_api/settings.py
@@ -41,7 +41,6 @@ REST_FRAMEWORK = {
 if 'DEV' not in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 
 REST_USE_JWT = True
