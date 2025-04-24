1ee6279ae3b3b2ce84f4106d7787a9397d0fea14
BekMart <bekmart1989@gmail.com>
2025-04-24 10:03:12 +0100

    Add related name to comment model

--- comments/models.py
+++ comments/models.py
@@ -8,7 +8,9 @@ class Comment(models.Model):
     Comment model, related to User and Post
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comment'
+        )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
