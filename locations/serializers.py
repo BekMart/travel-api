8ab6b87a1b7770d20e59c1aea70f8610b2d6a97a
BekMart <bekmart1989@gmail.com>
2025-04-29 21:07:54 +0100

    Return location name capitalised

--- locations/serializers.py
+++ locations/serializers.py
@@ -7,8 +7,12 @@ class LocationSerializer(serializers.ModelSerializer):
     Serializer for the Location model
     """
     posts_count = serializers.IntegerField(read_only=True)
+    name = serializers.SerializerMethodField()
 
     class Meta:
         model = Location
         fields = ['id', 'name', 'slug', 'image', 'description', 'posts_count']
         read_only_fields = ['slug']
+
+    def get_name(self, obj):
+        return obj.name.title()
