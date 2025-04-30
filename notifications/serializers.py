94cf3149df9a6fdc108a8fcf6c723accd2c2637a
BekMart <bekmart1989@gmail.com>
2025-04-30 14:18:11 +0100

    Add followers id to redirect using id/not username

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -4,6 +4,9 @@ from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
     from_user = serializers.StringRelatedField(read_only=True)
+    from_user_profile_id = serializers.IntegerField(
+        source='from_user.profile.id', read_only=True
+    )
     to_user = serializers.StringRelatedField(read_only=True)
     post = serializers.PrimaryKeyRelatedField(read_only=True)
     comment = serializers.PrimaryKeyRelatedField(read_only=True)
@@ -13,6 +16,7 @@ class NotificationSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'from_user',
+            'from_user_profile_id',
             'to_user',
             'notification_type',
             'post',
